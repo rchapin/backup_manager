905424fdea37204ecfe52f0496d09b2bdcc4c378
Ryan Chapin <rchapin@nbinteractive.com>
2021-02-06 22:24:34 -0500

    feature/initial-development: WIP

---
+++ backupmanager/lib/backupmanager.py
@@ -0,0 +1,84 @@
+import signal
+from backupmanager.lib.utils import Utils
+from apscheduler import events
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+class BackupManager(object):
+
+    def __init__(self, args, logger):
+        self.logger = logger
+        self.configs = Utils.load_configs(args.configfile)
+        self.scheduler = BackgroundScheduler()
+        self.scheduler.add_listener(self.event_listener)
+        self.running = False
+        self.shutdown = False
+        self.runonce = False
+        self.cron_schedule = self.configs['cron_schedule']
+
+    def event_listener(self, event):
+        if event.code == events.EVENT_JOB_ADDED:
+            for job in self.scheduler.get_jobs():
+                if event.code != events.EVENT_JOB_EXECUTED:
+                    self.logger.info(f'Job id={job.id}, scheduled for next run at {job.next_run_time}')
+
+        if self.runonce and event.code == events.EVENT_JOB_EXECUTED:
+            if self.scheduler.running:
+                self.logger.info('Shutting down scheduler')
+                self.scheduler.shutdown(wait=False)
+
+
+    def run(self):
+        # Register for the SIGTERM signal so that we can cleanly shutdown
+        signal.signal(signal.SIGTERM, self.signal_handler)
+        self.scheduler.start()
+        if self.runonce:
+            self.schedule_runonce_job()
+        else:
+            self.schedule_cron_job()
+
+        while self.scheduler.running:
+            self.scheduler._thread.join(0.1)
+        self.logger('BackupManager exiting run')
+
+    def exec_job(self):
+        self.logger.info('Starting exec_job....')
+        if self.running:
+            self.logger.warning(f'Unable to schedule multiple...')
+            return
+
+        self.running = True;
+
+        # Iterate over all of the sync_jobs.
+        for sync_job in self.configs['sync_jobs']:
+            self.logger.info(f'Executing sync_job={sync_job}')
+            '''
+            First, check to see if there is a 'blocks_on' key defined for this job.
+            If so, ensure that there is no pid file with an running process on the
+            defined host.
+            '''
+            if 'blocks_on' in sync_job:
+                blocks_on = sync_job['blocks_on']
+                self.logger.info(f'Validating blocks_on={blocks_on}')
+            pass
+
+
+        self.running = False;
+
+    def schedule_runonce_job(self):
+        if not self.running:
+            self.logger.info(f'Scheduling a runonce job')
+            self.scheduler.add_job(
+                max_instances=1,
+                id='runonce',
+                func=self.exec_job)
+
+    def schedule_cron_job(self):
+        self.logger.info(f'Scheduling cron job with schedule={self.cron_schedule}')
+        self.scheduler.add_job(
+            func=self.exec_job,
+            trigger=CronTrigger.from_crontab(self.cron_schedule))
+
+    def signal_handler(self, signal, frame):
+        self.logger(f'Handling signal={signal}, frame={frame}')
+        self.scheduler.shutdown(wait=True)
---
+++ backupmanager/lib/utils.py
@@ -0,0 +1,37 @@
+import yaml
+from fabric import Connection
+
+class Utils(object):
+
+    @staticmethod
+    def load_configs(configfile):
+        with open(configfile, 'r') as fh:
+            return yaml.load(fh, Loader=yaml.FullLoader)
+
+    @staticmethod
+    def is_blocked(blocks_on_conf, logger):
+        conn = Connection(
+            host=blocks_on_conf['host'],
+            user=blocks_on_conf['user'] if blocks_on_conf['user'] else 'root',
+            port=blocks_on_conf['port'] if blocks_on_conf['port'] else 22)
+        # Does the pip file exist?
+        result = conn.run(f"ls {blocks_on_conf['pid_path']}", warn=True)
+        if not result.ok():
+            '''
+            The file did not exist in the expected path. We will assume that
+            the process on which we were to block is not running.
+            '''
+            return False
+        else:
+            '''
+            The file does exist.  We need to read the contents of it and
+            confirm if the process listed in it is still running
+            '''
+            result = conn.run(f"cat {blocks_on_conf['pid_path']}", warn=True)
+            if not result.ok():
+                # FIXME
+                pass
+            else:
+                pid = result.stdout
+                logger.info(f'pid={pid}')
+
