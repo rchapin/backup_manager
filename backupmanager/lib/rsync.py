5c02f303a58e023761378b239810adeebc3b5f81
Ryan Chapin <rchapin@nbinteractive.com>
2021-08-15 07:16:15 -0400

    feature/initial-development: Added logging to rsync.py

--- backupmanager/lib/rsync.py
+++ backupmanager/lib/rsync.py
@@ -1,12 +1,19 @@
+import logging
+import sys
 
+logging.basicConfig(
+    format='%(asctime)s,%(levelname)s,%(module)s,%(message)s',
+    level=logging.INFO,
+    stream=sys.stdout)
 
 class Rsync(object):
 
     def __init__(self, args):
-        pass
+        logging.getLogger().setLevel(args['loglevel'].upper())
 
     def linux_native(self):
         pass
 
     def rsync_manager(self):
+        # TODO:
         pass
